5facb40b1c99dabb624823e5af0eae16b448e4de
Akumbom Wesley <akumbomwesley7@gmail.com>
2024-09-18 16:13:24 +0100

    include ticket_id in raffle serializer

--- apps/raffles/serializers.py
+++ apps/raffles/serializers.py
@@ -1,8 +1,11 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from .models import Raffle
 
 
 class RaffleSerializer(ModelSerializer):
+    ticket_id = serializers.PrimaryKeyRelatedField(source='ticket', read_only=True)
+
     class Meta:
         model = Raffle
-        fields = ['id', 'name', 'num_winners', 'prize_name', 'prize_img']
+        fields = ['id', 'ticket_id', 'name', 'num_winners', 'prize_name', 'prize_img']
